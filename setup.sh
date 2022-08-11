9fe1d31deb8a74932d7f2fa316f1bb95971a242f
Md.Abdulla Al Riad <70167682+Riad889@users.noreply.github.com>
2022-08-11 22:49:56 +0600

    Changed

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
 mkdir -p ~/.streamlit/
-echo "
-[general]n
-email = "riad1803153@gmail.com"n
-" > ~/.streamlit/credentials.toml
-echo "
-[server]n
-headless = truen
-enableCORS=falsen
-port = $PORTn
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
